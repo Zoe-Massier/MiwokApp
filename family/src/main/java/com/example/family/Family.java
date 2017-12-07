28469f46a1b7ba091a8d74b3e1197634241b2d8a
Zoe-Massier <zermassier@gmail.com>
2017-12-07 10:23:17 -0600

    12/7/17

--- family/src/main/java/com/example/family/Family.java
+++ family/src/main/java/com/example/family/Family.java
@@ -9,5 +9,7 @@ public class Family extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+
+        LinearLayout rootView =(LinearLayout) findViewById(R.id.rootView);
     }
 }

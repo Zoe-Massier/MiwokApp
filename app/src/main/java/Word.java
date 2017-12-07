a721704d96bd9c9b527ea6cccd46d1e2f3743814
Zoe-Massier <zermassier@gmail.com>
2017-12-07 10:35:04 -0600

    12/7/17

---
+++ app/src/main/java/Word.java
@@ -0,0 +1,6 @@
+/**
+ * Created by 592506 on 12/7/2017.
+ */
+
+public class Word {
+}
--- numbers/src/main/java/com/example/numbers/Numbers.java
+++ numbers/src/main/java/com/example/numbers/Numbers.java
@@ -9,5 +9,18 @@ public class Numbers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList <Word> words = new ArrayList<Word>();
+
+        words.add(new Word("one", "lutti"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kanekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo'e"));
+        words.add(new Word("ten", "na'aacha"));
     }
 }
